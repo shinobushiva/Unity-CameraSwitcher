bd2bf64aab8d185e71417c720f8e63b1edcd1287
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2015-07-13 02:40:24 +0900

    first commit

---
+++ CameraSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitcher :  MonoBehaviour
+{
+	
+	public SwitchableCamera[] cameras;
+	[HideInInspector]
+	public SwitchableCamera currentActive;
+
+	void Start ()
+	{
+		foreach (SwitchableCamera sc in cameras) {
+				sc.On(false);
+		}
+		cameras [0].On (true);
+		currentActive = cameras [0];
+
+	}
+
+	void Update(){
+	
+	}
+	
+	public void Switch (SwitchableCamera c)
+	{ 
+		print ("Switch:" + c.gameObject);
+		currentActive = c;
+
+		foreach (SwitchableCamera sc in cameras) {
+			if(sc == c){
+				sc.On(true);
+			}else{
+				sc.On(false);
+			}
+		}
+
+	}
+}
---
+++ SwitchableCamera.cs
@@ -0,0 +1,36 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class SwitchableCamera : MonoBehaviour
+{
+	public Camera c;
+
+	void Awake(){
+	}
+	
+	void Start ()
+	{
+		if (c == null) {
+			c = GetComponentInChildren<Camera>();
+		}
+
+		if (c == null) {
+			print ("Could not find any camera in children. The component is destroied");
+			Destroy(this);
+		}
+
+	}
+
+	public void On(bool f){
+		Behaviour[] mbs = GetComponentsInChildren<Behaviour> ();
+		foreach (Behaviour mb in mbs) {
+			if(mb == null)
+				continue;
+
+			mb.enabled = f;
+		}
+		//enabled = true;
+	}
+ 
+
+}
