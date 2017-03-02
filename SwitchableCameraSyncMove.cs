a177a6d718b3852193791d457cc2765864729914
Shinobu Izumi <stagesp1@gmail.com>
2017-03-03 01:59:53 +0900

    switchable camera sync move

---
+++ SwitchableCameraSyncMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Shiva.CameraSwitch;
+
+public class SwitchableCameraSyncMove : MonoBehaviour {
+	
+	public Vector3 offset;
+	public Vector3 rotation;
+
+	public CameraSwitcher switcher;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = switcher.CurrentActive.transform.position+offset;
+		transform.rotation = switcher.CurrentActive.transform.rotation;
+		Quaternion q = transform.rotation;
+		Vector3 ang = q.eulerAngles;
+		ang += rotation;
+		q.eulerAngles = ang;
+		transform.rotation = q;
+	}
+}
